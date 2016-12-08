5270938d2e3db40a4719a643df60abd256a60dc0
Abdullah2Cool <abdullahbinasad14@gmail.com>
2016-12-08 12:28:07 -0500

    Initial Commit

---
+++ desktop/src/scratch/box2d/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package scratch.box2d.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import scratch.box2d.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
