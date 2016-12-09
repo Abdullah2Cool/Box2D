7f99697a0ee954479c7ee6f18a380f249530e694
Abdullah2Cool <abdullahbinasad14@gmail.com>
2016-12-08 21:42:22 -0500

    Box2D Tutorial 1 implemented.

--- core/src/scratch/box2d/Main.java
+++ core/src/scratch/box2d/Main.java
@@ -5,6 +5,11 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.physics.box2d.Body;
@@ -20,43 +25,65 @@ public class Main extends ApplicationAdapter {
     World world;
     Body player, platform;
     final float PPM = 32;
+    final float SCALE = 2;
+    SpriteBatch batch;
+    Texture texture;
+    OrthogonalTiledMapRenderer tiledMapRenderer;
+    TiledMap map;
 
     @Override
     public void create() {
+        float width = Gdx.graphics.getWidth();
+        float height = Gdx.graphics.getHeight();
         camera = new OrthographicCamera();
-        camera.setToOrtho(false, Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
+        camera.setToOrtho(false, width / SCALE, height / SCALE);
 
         world = new World(new Vector2(0, -9.8f), false);
         b2dr = new Box2DDebugRenderer();
-        player = createBox(8, 10, 32, 32, false);
+        player = createBox(0, 8, 32, 32, false);
         platform = createBox(0, 0, 64, 32, true);
-    }
 
+        batch = new SpriteBatch();
+        texture = new Texture("geoDash.png");
+
+        map = new TmxMapLoader().load("map.tmx");
+    }
 
     @Override
     public void render() {
-        update(Gdx.graphics.getDeltaTime());
+        update();
 
         // render
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        batch.begin();
+        batch.draw(texture, player.getPosition().x * PPM - 16, player.getPosition().y * PPM - 16);
+        batch.end();
         b2dr.render(world, camera.combined.scl(PPM));
     }
 
-    private void update(float deltaTime) {
+    @Override
+    public void dispose () {
+        b2dr.dispose();
+        batch.dispose();
+        texture.dispose();
+    }
+
+    private void update() {
         world.step(1 / 60f, 6, 2);
         inputUpdate();
         updateCamera();
+        batch.setProjectionMatrix(camera.combined);
     }
 
     private void inputUpdate() {
         int horizontalforce = 0;
         if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-            horizontalforce += 1;
+            horizontalforce -= 1;
         }
         if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-            horizontalforce -= 1;
+            horizontalforce += 1;
         }
         if (Gdx.input.isKeyJustPressed(Input.Keys.UP)) {
             player.applyForceToCenter(0, 300, false);
@@ -64,7 +91,7 @@ public class Main extends ApplicationAdapter {
         player.setLinearVelocity(horizontalforce * 5, player.getLinearVelocity().y);
     }
 
-    private Body createBox(int x, int y, int width, int height, boolean isStatic) {
+    private Body createBox(float x, float y, int width, int height, boolean isStatic) {
         Body pBody;
         BodyDef def = new BodyDef();
         if (isStatic) {
@@ -72,7 +99,7 @@ public class Main extends ApplicationAdapter {
         } else {
             def.type = BodyDef.BodyType.DynamicBody;
         }
-        def.position.set(x / PPM, 0 / PPM);
+        def.position.set(x / PPM, y / PPM);
         pBody = world.createBody(def);
 
         PolygonShape shape = new PolygonShape();
@@ -89,16 +116,5 @@ public class Main extends ApplicationAdapter {
         camera.position.set(position);
 
         camera.update();
-    }
-
-    @Override
-    public void resize(int width, int height) {
-        camera.setToOrtho(false, width / 2, height / 2);
-    }
-
-    @Override
-    public void dispose() {
-        world.dispose();
-        b2dr.dispose();
     }
 }
